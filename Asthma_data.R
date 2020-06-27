1fc61d994ed5a894e63514be8ecb20d27b98c614
ElizaCanadian <59021292+ElizabethSeidle@users.noreply.github.com>
2020-06-27 10:29:42 -0400

    Updated merge code

--- Asthma_data.R
+++ Asthma_data.R
@@ -10,7 +10,7 @@ asthma = read_excel("Asthma_data.xlsx")
 ntatranslation = read_excel("NTA_translation_2_Ashtma.xlsx")
 
 for (i in 1:nrow(ntatranslation)){
-  asthma$Geography[asthma$Geography == ntatranslation$Raw_NTA[i]] = ntatranslation$NTA[i]
+  asthma$Geography[asthma$Geography == ntatranslation$Raw_NTA[i]] = ntatranslation$Small_NTA[i]
 } 
 
 write.csv(asthma, "asthma_by_NTA.csv")
